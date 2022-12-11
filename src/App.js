b5a64a90c0f2f1c54261af8dea4f45d95dc43415
Nahid-07 <nahidasif1998@gmail.com>
2022-12-11 18:59:07 +0600

    project details added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, RouterProvider } from "react-router-dom";
 import Aboutme from "./aboutme/Aboutme";
 import Form from "./Form/Form";
 import Hero from "./Hero/Hero";
 import Navbar from "./navbar/Navbar";
+import Details1 from "./projectDetails/Details1";
+import Details2 from "./projectDetails/Details2";
+import Details3 from "./projectDetails/Details3";
 import Projects from "./Projects/Projects";
+import { router } from "./routes/Routes";
 import SkillsBanner from "./Skillsbanner/SkillsBanner";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar></Navbar>
-      <Hero></Hero>
-      <SkillsBanner></SkillsBanner>
-      <Aboutme></Aboutme>
-      <Projects></Projects>
-      <Form></Form>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <Hero></Hero>
+        <SkillsBanner></SkillsBanner>
+        <Aboutme></Aboutme>
+        <Projects></Projects>
+        <Form></Form>
+      </BrowserRouter>
+      <Details1></Details1>
+      <Details2></Details2>
+      <Details3></Details3>
+    </>
   );
 }
 
--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 const Form = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_htz18n8', 'template_q8lg3oa', form.current, 'QgfKRvDyGdxr1iWcp')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+      };
+
     return (
         <div id='contact' className=' bg-slate-500 p-20'>
             <h1 className='text-5xl text-center font-bold mb-10 text-orange-400'>Contact me</h1>
-            <form action="" className='container mx-auto flex flex-col md:w-3/5 gap-5'>
+            <form ref={form} onSubmit={sendEmail} className='container mx-auto flex flex-col md:w-3/5 gap-5'>
             <div>
             <input type="text" placeholder="Your Name" className="input input-bordered w-full" />
             <input type="text" placeholder="Your Email" className="input input-bordered w-full my-5" />
